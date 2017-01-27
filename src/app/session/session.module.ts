c7214206c0a47953601d62a00fad0bed30c40f4a
Jon Kruger <github@jonkruger.com>
2017-01-26 21:55:20 -0500

    checkpoint

--- src/app/session/session.module.ts
+++ src/app/session/session.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SessionListComponent } from './session-list/session-list.component';
 import { SessionDetailComponent } from './session-detail/session-detail.component';
+import { SessionService } from './session.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SessionListComponent, SessionDetailComponent]
+  declarations: [SessionListComponent, SessionDetailComponent],
+  providers: [SessionService]
 })
 export class SessionModule { }
